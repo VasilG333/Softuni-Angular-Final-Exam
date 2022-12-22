a2e7bb5fd15969bd1744e1732ce020eba6ad736e
VasilG333 <107360902+VasilG333@users.noreply.github.com>
2022-12-22 22:19:58 +0200

    Making progress
    work work work

---
+++ cars-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './main/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  }, 
+  {
+    path: 'auth/login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
