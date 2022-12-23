ff95dd18d9cfb6db316fcdb3788efe2c36914008
VasilG333 <107360902+VasilG333@users.noreply.github.com>
2022-12-23 05:04:33 +0200

    yes

--- cars-app/src/app/auth/register/register.component.ts
+++ cars-app/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  constructor(private authService: AuthService, private route: Router) {}
+  registerHandler() {
+    this.route.navigate([''])
+  }
 }
